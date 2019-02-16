46ac455a5457e2a0f086f18d5b8922f58a37a7c6
rcallan13 <ron_allan@yahoo.com>
2019-02-15 22:13:58 -0800

    Initial Commit

---
+++ AutoSearchKitTests/AutoSearchKitTests.swift
@@ -0,0 +1,34 @@
+//
+//  AutoSearchKitTests.swift
+//  AutoSearchKitTests
+//
+//  Created by Ron Allan on 2019-02-15.
+//  Copyright © 2019 RCAL Software Solutions. All rights reserved.
+//
+
+import XCTest
+@testable import AutoSearchKit
+
+class AutoSearchKitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
